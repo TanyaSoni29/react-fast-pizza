2fe7d4f58681bd89335c26691d9427e9b3a1054e
unknown <tanu139sona@gmail.com>
2025-06-07 15:03:16 +0530

    Applayout integration

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,13 @@
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <Header />
+      <main>
+      <h1>Content</h1></main>
+      <CartOverview />
+    </div>
+  )
+}
