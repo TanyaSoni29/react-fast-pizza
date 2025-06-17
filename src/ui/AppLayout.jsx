af0d227f94c23c40a53819608537f9d1ced56159
TanyaSoni29 <tanu139sona@gmail.com>
2025-06-17 13:37:08 +0530

    console

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,7 +5,6 @@ import Loader from './Loader';
 
 export default function AppLayout() {
   const navigation = useNavigation();
-  console.log(navigation);
   const isLoading = navigation.state === 'loading';
   return (
     <div className="grid h-screen grid-cols-1 grid-rows-[auto_1fr_auto]">
