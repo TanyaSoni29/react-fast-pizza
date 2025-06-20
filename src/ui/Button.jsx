75b89a5e5cbfead5c235da1fc6ecaae00041ef63
TanyaSoni29 <tanu139sona@gmail.com>
2025-06-20 18:57:47 +0530

    comments

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 export default function Button({ children, disabled, to, type }) {
+  // in this way we define the Button Component and Use it whenever we want to use it
   const base =
     'inline-block text-sm rounded-full bg-yellow-400  font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed';
   const styles = {
